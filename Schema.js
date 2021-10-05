f235f013614334f12c830197404b6e976e8c6d19
Jack Hou <jackhou147@berkeley.edu>
2021-10-05 10:49:02 -0700

    schema class started

---
+++ Schema.js
@@ -0,0 +1,23 @@
+class Schema{
+    #statement; //String
+
+    constructor(){
+
+    }
+
+    Equals(){
+        
+    }
+}
+
+class Conditional extends Schema{
+    #antecedent; //String
+    #consequence; //String
+
+    constructor(){
+        super();
+
+    }
+
+
+}
